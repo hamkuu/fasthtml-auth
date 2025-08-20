68870df4eca5bb10331ed7304269c7307eb9f5a3
Hirokoma Shaw <hamkuu@gmail.com>
2025-08-20 14:00:24 +0900

    Introduce MonsterUI

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fasthtml.common import *
+from monsterui.all import *
 from fasthtml.oauth import GoogleAppClient, OAuth
 from fastlite import database
 from starlette.responses import RedirectResponse
 
 import os
 
-app, rt = fast_app()
+app, rt = fast_app(hdrs=Theme.blue.headers(), live=True)
 
 
 class User:
@@ -24,22 +25,51 @@ class Auth(OAuth):
         user = db.users("oauth_id=?", (sess["auth"],))
         if not user:
             db.users.insert(User(oauth_id=sess["auth"], email=info.email))
-        return RedirectResponse("/dashboard", status_code=303)
+        return RedirectResponse("/home", status_code=303)
 
 
 client = GoogleAppClient(os.getenv("GOOGLE_CLIENT_ID"), os.getenv("GOOGLE_CLIENT_SECRET"))
 oauth = Auth(app, client, skip=("/", "/logout", "/redirect"), login_path="/")
 
 
+def ex_navbar1():
+    return NavBar(
+        A("Home", href="/home"),
+        A("Theme", href="/theme"),
+        A("Logout", href="/logout"),
+        brand=H3("FastHTML"),
+    )
+
+
 @rt
 def index(req):
-    return Titled("FastHTML App", A("Login", href=oauth.login_link(req)))
+    return Center(
+        DivVStacked(
+            H1("Welcome"),
+            A(UkIcon("log-in"), "Login with Google", href=oauth.login_link(req), cls=(ButtonT.primary, "btn")),
+            Small(
+                A(cls=AT.muted, href="#demo")("Terms of Service"),
+                cls=(TextT.muted, "text-center"),
+            ),
+        ),
+        cls="h-screen",
+    )
 
 
 @rt
-def dashboard(sess):
+def home(sess):
     user = db.users("oauth_id=?", (sess["auth"],))[0]
-    return Titled("Dashboard", P(f"Welcome back {user.email}"), A("Logout", href="/logout"))
+    return ex_navbar1(), Titled("Home", P(f"Welcome {user.email}"), A("Logout", href="/logout"))
+
+
+@rt
+def theme():
+    return Container(
+        DivVStacked(
+            A(UkIcon("arrow-left"), "Back", href="javascript:history.back()", cls=(ButtonT.secondary, "btn")),
+            ThemePicker(color=True, radii=True, shadows=True, font=True, mode=True, cls="p-4", custom_themes=[]),
+        )
+    )
 
 
 serve()
